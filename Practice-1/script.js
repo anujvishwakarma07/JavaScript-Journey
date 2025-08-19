2bde8b9e0a631a160ee999cbbd0f874b1aec1ee5
Anuj Vishwakarma <anujvishwakarma7077@gmail.com>
2025-08-20 00:11:50 +0530

    First Exersise Completed

---
+++ Practice-1/script.js
@@ -0,0 +1,23 @@
+//Question - 1
+let name = "Anuj Vishwakarma";
+console.log(name.length);
+
+//Question - 2
+let firstName = "Anuj";
+console.log(firstName.charAt(0));
+console.log(firstName[0]);
+console.log(firstName.at(0));
+
+//Question - 3
+console.log(firstName.at(-1));
+console.log(firstName.length - 1);
+
+// Question - 4 => Declear the output of the following code - "apnacollage"+123
+console.log("apnacollage"+123);
+
+//Question - 5
+let a = "";
+console.log(a.length);
+
+let b = " ";
+console.log(b.length);
