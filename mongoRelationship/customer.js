eca859680e3c3472449f6ac48fcabb08489738da
anujvishwakarma07 <anujvishwakarma7077@gmail.com>
2026-02-19 22:14:47 +0530

    we have learn about how to handle the deletions in mongoDB

--- mongoRelationship/customer.js
+++ mongoRelationship/customer.js
@@ -30,35 +30,51 @@ const orderSchema = new mongoose.Schema({
     ]
 })
 
+orderSchema.post("findOneAndDelete", async (customer) => {
+    if (customer.orders.length) {
+        let result = await Order.deleteMany({ _id: { $in: customer.orders } });
+        console.log(result);
+    }
+    // console.log(customer.orders);
+
+
+});
+
+// orderSchema.post("findOneAndDelete", async ()=>{
+
+// })
+
 const Order = mongoose.model("Order", customerSchema);
 const Customer = mongoose.model("customer", orderSchema);
 
-const addCustomer = async () => {
-    // let customer1 = new Customer({
-    //     name: "Raju Shrivastava",
-    // });
-      
-
-    // const order1 = await Order.findOne({
-    //     item: "Samosa"
-    // })
-    // const order2 = await Order.findOne({
-    //     item: "Chola bhatura"
-    // })
 
 
-    // customer1.orders.push(order1);
-    // customer1.orders.push(order2);
+// const addCustomer = async () => {
+//     let customer1 = new Customer({
+//         name: "Raju Shrivastava",
+//     });
 
-    // let result = await customer1.save();
-    // console.log(result);
 
-    const result = await Customer.find({}).populate("orders");
-    console.log(result[0]);
-    
-}
+//     const order1 = await Order.findOne({
+//         item: "Samosa"
+//     })
+//     const order2 = await Order.findOne({
+//         item: "Chola bhatura"
+//     })
 
-addCustomer();
+
+//     customer1.orders.push(order1);
+//     customer1.orders.push(order2);
+
+//     let result = await customer1.save();
+//     console.log(result);
+
+//     // const result = await Customer.find({}).populate("orders");
+//     // console.log(result[0]);
+
+// }
+
+// addCustomer();
 
 // const addOrder = async () => {
 //     const result = await order.insertMany([{
@@ -79,3 +95,47 @@ addCustomer();
 // }
 
 // addOrder();
+
+// const addCust = async () => {
+//     const newCust = new Customer({
+//         name: "Mahendra Singh Dhoni",
+//     });
+
+//     const newOrder = await Order.insertMany([
+//         {
+//             item: "Malai kopta",
+//             price: 250,
+//         },
+//         {
+//             item: "Ras Malai",
+//             price: 208,
+//         },
+//         {
+//             item: "Sahi paneer",
+//             price: 50,
+//         },
+//     ])
+
+//     newCust.orders.push(...newOrder.map(order => order._id));
+
+//     // await newOrder.save();
+//     const result = await newCust.save();
+
+//     console.log("Added Customer : ", result);
+// }
+
+// addCust();
+
+// const usePopulate = async () =>{
+//     const result = await Customer.find({}).populate("orders");
+//     console.log(result[1].orders);
+// }
+
+// usePopulate();
+
+const deleteCustomer = async () => {
+    const deletedCust = await Customer.findByIdAndDelete('699714b675798c39c410674b');
+    console.log(deletedCust);
+}
+
+deleteCustomer();
