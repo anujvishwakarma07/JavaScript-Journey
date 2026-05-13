01333b4b550072422be4d3247551d8df134a9811
anujvishwakarma07 <anujvishwakarma7077@gmail.com>
2026-05-13 17:04:02 +0530

    mostlally react is completed a this gap is for projects and interview prepairations, remaining course will complete from today

--- mongoRelationship/expressRouter/index.js
+++ mongoRelationship/expressRouter/index.js
@@ -1,18 +1,44 @@
 const express = require("express");
 const app = express();
 const user = require("./routes/user");
-const post  = require("./routes/post");
+const post = require("./routes/post");
+const cookeParser = require("cookie-parser");
 
 
 const port = 3000;
+
+app.use(cookeParser("secretcode"));
 app.use("/user", user);
 app.use("/post", post);
 
+app.get("/", (req, res) => {
+    // console.log(req.cookies);
+    const { name = "anonymouse" } = req.cookies;
+    res.send(`Hi I am , ${name}`);
+})
+
+app.get("/secretcookies", (req, res) => {
+    res.cookie("madein", "India", { signed: true });
+    res.send("signed cookies send");
+})
+app.get("/verify", (req, res) => {
+    console.log(req.cookies);
+    console.log(req.signedCookies);
+})
+
+
+
+
+app.get("/setcookies", (req, res) => {
+    res.cookie("Greet", "Namaste");
+    res.send("send some cookies");
+})
 
 
 
 
 
-app.listen(port, ()=>{
+
+app.listen(port, () => {
     console.log(`app is listening on the port : ${port}`)
 })
