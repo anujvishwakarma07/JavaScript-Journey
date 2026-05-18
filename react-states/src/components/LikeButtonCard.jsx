a78bb2a765c50c8bb21a9e89c6fc8a17363e5f8c
anujvishwakarma07 <anujvishwakarma7077@gmail.com>
2026-05-18 12:41:23 +0530

    learn about how to use functions as a initial state and re -rendering stratagies and callbacks

--- react-states/src/components/LikeButtonCard.jsx
+++ react-states/src/components/LikeButtonCard.jsx
@@ -24,20 +24,49 @@
 // export default LikeButtonCard       
 
 
+// // Componenet re-rendering
+// import React, { useState } from 'react'
 
-import React, { useState } from 'react'
+// const LikeButtonCard = () => {
+//     const [count, setCount] = useState(0);
+//     console.log("Component is re-rendered");
+//     console.log(`count : ${count}`);
+//     const counter = () => {
+//         setCount(count + 1);
+//         console.log(`new value of count : ${count}`);
+//     }
+//   return (
+//     <div>
+//         <p>count : {count}</p>
+//         <button onClick={counter}>increase count : {count}</button>
+//     </div>
+//   )
+// }
+
+// export default LikeButtonCard
+
+import { useState } from "react";
+
+import React from 'react'
 
 const LikeButtonCard = () => {
-    const [count, setCount] = useState(0);
-    console.log("Component is re-rendered");
-    console.log(`count : ${count}`);
-    const counter = () => {
-        setCount(count + 1);
-        console.log(`new value of count : ${count}`);
-    }
+  // const [count, setCount] = useState(0);
+  // we can also pass callback function as a initial state
+  const random = () => {
+    console.log('random is exicuted');
+    return Math.random();
+  }
+  const [count , setCount] = useState(random);
+  const counter = () => {
+    setCount((currCount)=> {
+      return currCount + 1;
+    })
+
+  }
   return (
     <div>
-        
+      <p>Count is : {count}</p>
+      <button onClick={counter}>increase count : {count}</button>
     </div>
   )
 }
